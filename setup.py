254845a1b82c33a88b8170173ff49c6c077e1d27
Aiden Nibali <dismaldenizen@gmail.com>
2017-12-20 12:11:19 +1100

    v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tele',
-    version='0.1.0a6',
+    version='0.2.0',
     packages=find_packages(),
     author='Aiden Nibali',
     description='Telemetry for PyTorch',
