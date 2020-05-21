c128f0ad2fd6aa27936c792b4ddfc2aeef1f152b
Aiden Nibali <dismaldenizen@gmail.com>
2020-05-21 10:59:28 +1000

    v0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tele',
-    version='0.2.2',
+    version='0.2.3',
     packages=['tele', 'tele.console', 'tele.folder', 'tele.showoff',
               'tele.sacred'],
     author='Aiden Nibali',
