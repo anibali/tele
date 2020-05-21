c5560a5a7c453ab9f9520b8a372656b5eee8e33b
Aiden Nibali <dismaldenizen@gmail.com>
2020-05-21 11:16:34 +1000

    Option for custom write function in console output

--- tele/console/__init__.py
+++ tele/console/__init__.py
@@ -2,12 +2,27 @@ from tele.meter import ValueMeter, TimeMeter, MeanValueMeter, MedianValueMeter
 import tele.console.views
 
 
+def _default_write_fn(step_num, rendered):
+    print('[{:4d}] '.format(step_num) + ', '.join(rendered))
+
+
 class Sink(tele.Sink):
+    def __init__(self, cell_list, write_fn=None):
+        super().__init__(cell_list)
+        if write_fn is None:
+            self._write_fn = _default_write_fn
+        else:
+            self._write_fn = write_fn
+
     def write(self, step_num, rendered):
-        print('[{:4d}] '.format(step_num) + ', '.join(rendered))
+        self._write_fn(step_num, rendered)
 
 
 class Conf(tele.Conf):
+    def __init__(self, write_fn=None):
+        super().__init__()
+        self._write_fn = write_fn
+
     def make_auto_view(self, meter_name, meter):
         if isinstance(meter, MeanValueMeter) \
                 or isinstance(meter, MedianValueMeter) \
@@ -18,4 +33,4 @@ class Conf(tele.Conf):
 
     def build(self, view_list):
         cell_list = [view.build() for view in view_list]
-        return Sink(cell_list)
+        return Sink(cell_list, self._write_fn)
